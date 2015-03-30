553aab8c6e791e3a504116914f5cf01a6a9a72a0
Haoliang <A30041839@gmail.com>
2015-03-30 18:03:35 +0800

    update

--- 1.cpp
+++ 1.cpp
@@ -5,40 +5,24 @@ using namespace std;
 class Solution {
 public:
   vector<int> twoSum(vector<int> &numbers, int target) {
+    return twoSum1(numbers, target);
+  }
+
+  vector<int> twoSum1(vector<int> &numbers, int target) {
     vector<int> res;
-    vector<int> nums = numbers;
-    sort(numbers.begin(), numbers.end());
-    for (int i = 0; i < numbers.size() - 1 and numbers[i] <= target / 2; ++i){
-      int pos = find(numbers, target - numbers[i], i + 1, numbers.size() - 1);
-      if (pos != -1){
-        getIndex(nums, res, numbers[i], numbers[pos]);
-        return res;
-      }
-    }
-    return res;
+    vector<int> numbers1 = numbers;
+    int len = numbers.size();
+    for (int i = 0; i < len; ++i) {
+      int num1 = numbers[i];
+      int num2 = target - num1;
   }
 
-  int find(vector<int>& numbers, int num, int low, int high){
-    while (low <= high){
-      int mid = (low + high) / 2;
-      if (numbers[mid] == num){
-        return mid;
-      }else if (num < numbers[mid]){
-        high = mid - 1;
-      }else{
-        low = mid + 1;
-      }
-    }
-    return -1;
+  vector<int> twoSum2(vector<int> &numbers, int target) {
   }
 
-  void getIndex(vector<int>& nums, vector<int>& res, int a, int b){
-    for (int i = 0; i < nums.size(); ++i){
-      if (nums[i] == a or nums[i] == b){
-        res.push_back(i + 1);
-      }
-    }
+  int find(vector<int>& numbers, int num, int low, int high) {
   }
+
 };
 
 int main(){
